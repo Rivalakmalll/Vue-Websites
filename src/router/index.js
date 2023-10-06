33f5283b093fe7df7ff24e8706b6f508866a491f
Rival Akmal <rivalakmal10@gmail.com>
2023-10-06 09:32:48 +0700

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    meta: { title: 'Home' }
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView,
+    meta: { title: 'About' }
+  },
+  {
+    path: '/portfolio',
+    name: 'portfolio',
+    component: () => import('../views/PortfolioView.vue'),
+    meta: { title: 'Portfolio' }
+  },
+  {
+    path: '/blog',
+    name: 'blog',
+    component: () => import('../views/BlogView.vue'),
+    meta: { title: 'Blog' }
+  },
+  {
+    path: '/read/:slug/:id',
+    name: 'articleDetail',
+    component: () => import('../views/ArticleView.vue'),
+    meta: { title: 'Blog' }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title + ' - portofolio'; // Mengambil judul dari meta atau mengatur judul default jika tidak ada
+  next();
+});
+
+export default router
